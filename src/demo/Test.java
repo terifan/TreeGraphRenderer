994382385579d692e3b4a6112891d96e031f70f0
terifan <pinjata69@gmail.com>
2022-09-23 13:08:34 +0200

    update

--- src/demo/Test.java
+++ src/demo/Test.java
@@ -16,8 +16,7 @@ public class Test
 		{
 			VerticalImageFrame frame = new VerticalImageFrame().setMargin(10);
 			frame.add(new TreeRenderer("{root}'A:B:C'#f00#a00#fff['D:E'###0f0,'F:G'#00f,{internal}'H:I'#0ff[{leaf}'J:K'#ff0,'L:M'#f0f]]").render(new VerticalLayout()));
-//			frame.add(new TextSlice("test", Color.RED, Color.WHITE, 20));
-			frame.add(new TextSlice("testTESTÅÄÖ"));
+			frame.add(new TextSlice("test", Color.RED, Color.WHITE, 20));
 			frame.add(new TreeRenderer("{root}'*:Jalapeno:Mango'[{first}'*:Dove'['Apple:Banana:Circus','Dove:Ear:Female'],'*:Japanese'['Gloves:Head:Internal','Japanese:Knife:Leap'],'*:Quality'['Mango:Nose:Open','Quality:Rupee']]").render(new HorizontalLayout()));
 			frame.add(new TreeRenderer("'*:Jalapeno:Mango'['*:Dove'['Apple:Banana:Circus','Dove:Ear:Female'],'*:Japanese'['Gloves:Head:Internal','Japanese:Knife:Leap'],'*:Open:Quality'['Mango:Nose','Open:Pen','Quality:Rupee']]").render(new VerticalLayout()));
 			frame.add(new TreeRenderer("['a','b','c','d']#f00").render(new HorizontalLayout()));
